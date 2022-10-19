16cea705abdddb1d4923f402d6179878e62c640e
harshit240 <601himanshusahu@gmail.com>
2022-10-19 11:27:07 +0530

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { FestivalsComponent } from './sec4/festivals/festivals.component';
-import { GeetaComponent } from './sec4/geeta/geeta.component';
-import { TempleComponent } from './sec4/temple/temple.component';
-import { VedasComponent } from './sec4/vedas/vedas.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { ShivjiComponent } from './shivji/shivji.component';
 
 const routes: Routes = [
@@ -13,25 +10,14 @@ const routes: Routes = [
     component:HomeComponent
   },
   {
-    path:'festivals',
-    component:FestivalsComponent
-  },
-  {
     path:'shivji',
     component:ShivjiComponent
   },
   {
-    path:'vedas',
-    component:VedasComponent
+    path:'**',
+    component:PagenotfoundComponent
   },
-  {
-    path:'temple',
-    component:TempleComponent
-  },
-  {
-    path:'geeta',
-    component:GeetaComponent
-  },
+
 ];
 
 @NgModule({
---
+++ src/app/pooja-religion/pooja-religion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pooja-religion',
+  templateUrl: './pooja-religion.component.html',
+  styleUrls: ['./pooja-religion.component.css']
+})
+export class PoojaReligionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
