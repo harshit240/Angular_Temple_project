4f5cbca5292c4d5973fe635c09b1974b8710aaab
harshit240 <601himanshusahu@gmail.com>
2022-10-19 15:07:23 +0530

    .

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"admin-dashboard",
+    component:DashboardComponent
+  },
+  {
+    path:"admin-sidebar",
+    component:SidebarComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { PoojaFestivalComponent } from './pooja-festival/pooja-festival.component';
+import { PoojaGeetaComponent } from './pooja-geeta/pooja-geeta.component';
+import { PoojaReligionComponent } from './pooja-religion/pooja-religion.component';
+import { PoojaTempleComponent } from './pooja-temple/pooja-temple.component';
+import { PoojaVedasComponent } from './pooja-vedas/pooja-vedas.component';
+import { PoojaComponent } from './pooja/pooja.component';
 import { ShivjiComponent } from './shivji/shivji.component';
 
 const routes: Routes = [
@@ -10,8 +16,28 @@ const routes: Routes = [
     component:HomeComponent
   },
   {
-    path:'shivji',
-    component:ShivjiComponent
+    path:'all',
+    component:PoojaComponent
+  },
+  {
+    path:'festival',
+    component:PoojaFestivalComponent
+  },
+  {
+    path:'geeta',
+    component:PoojaGeetaComponent
+  },
+  {
+    path:'religion',
+    component:PoojaReligionComponent
+  },
+  {
+    path:'temple',
+    component:PoojaTempleComponent
+  },
+  {
+    path:'vedas',
+    component:PoojaVedasComponent
   },
   {
     path:'**',
